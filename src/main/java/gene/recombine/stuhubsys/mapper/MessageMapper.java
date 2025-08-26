8295b657e3a74f888a242021e6218935167bd562
little-cow-angle <zhangming04822@foxmail.com>
2025-08-26 15:04:02 +0800

    消息模块bug修复

--- src/main/java/gene/recombine/stuhubsys/mapper/MessageMapper.java
+++ src/main/java/gene/recombine/stuhubsys/mapper/MessageMapper.java
@@ -11,7 +11,7 @@ import java.util.List;
 public interface MessageMapper{
     @Select("select count(*) from sign_up_record where status=1")
     Integer countNeedOperate();
-    @Select("select *from notice where type=#{type} and status=1 and expired_time > NOW()")
+    @Select("select *from notice where (type=#{type} or type=1) and status=1 and expired_time > NOW()")
     List<Notice> getNotice(Integer type);
     @Select("select path from notice_file where notice_id=#{id}")
     List<String> getFiles(int id);
